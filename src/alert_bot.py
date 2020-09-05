6527c1012ea60bf198048947982d97fc422daf9a
w1jtoo <wanadooht@gmail.com>
2020-09-05 23:00:52 +0500

    added white_list

--- src/alert_bot.py
+++ src/alert_bot.py
@@ -1,11 +1,29 @@
 from telepot import Bot
 
+WHITE_LIST = [ "w1jimtoo" ]
+SERVER_ADDRESS = ""
+bot = None
+
 def get_token() -> str:
     pass # TODO: get tokens from home server
 
-def handle_msg() ->  None: 
-    pass
+def update_whitelist() -> None:
+    global WHITE_LIST
+    pass # TODO: get whitelist
 
-def start_bot() -> None: 
-    bot = Bot(get_token())
+def handle_msg(msg: dict) ->  None:
+    chat_id = msg['chat']['id']
+    user_name = msg['from']['username']
+    if user_name not in WHITE_LIST:
+        bot.sendMessage(chat_id, 'Тебя нет в списке разрешённых пользователей :(')
+        bot.sendSticker(chat_id, 'CAACAgUAAxkBAAMLX1PI2-TOAaXkLvJpyjD3JQzkAAHjAAKfAAPceAwIkmRBx7ks9FgbBA')
+        bot.sendMessage(chat_id, '''Автор бота: @w1jimtoo
+Код бота: https://github.com/w1jtoo/ubiquitous-telegram-alert
+
+''')
+        return
+
+def start_bot() -> None:
+    global bot
+    bot = Bot()
     bot.message_loop(handle_msg, run_forever=True)
--- src/utelal.py
+++ src/utelal.py
@@ -4,11 +4,19 @@ from argparse import ArgumentParser
 def main() -> None: 
     parser = ArgumentParser(
             description="",
-            usage="utelal.py -s")
-
-    parser.add_argument(
-            "--start", "starts bot"
+            usage="utelal.py start"
             )
+    
+    subparser = parser.add_subparsers(dest="command")
+    subparser.add_parser(
+            "start", help="starts bot"
+            )
+
+    args = parser.parse_args()
+    if args.command == "start":
+        start_bot()
+    else:
+        parser.print_usage()
 
 if __name__ == "__main__":
     main()
